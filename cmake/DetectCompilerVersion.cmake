3ce2eae4439e1e14f8b08d3f17dc39d3d799a1e8
Nitin Bhat <nbhat4@illinois.edu>
2020-11-06 09:12:14 -0800

    Changes to get the build working on Cori
    Change-Id: I0153bab49ee386572151aa003ee31e543744db23

--- cmake/DetectCompilerVersion.cmake
+++ cmake/DetectCompilerVersion.cmake
@@ -13,10 +13,10 @@ if (DEFINED CMAKE_Fortran_COMPILER)
   list(APPEND COMPILERS "Fortran")
 endif()
 
-foreach(comp  ${COMPILERS})
-  string(REGEX MATCH "([0-9]*)\\.([0-9]*)\\.([0-9]*)"
-         major ${CMAKE_${comp}_COMPILER_VERSION})
-  set(CMAKE_${comp}_COMPILER_MAJOR ${CMAKE_MATCH_1})
-  set(CMAKE_${comp}_COMPILER_MINOR ${CMAKE_MATCH_2})
-  set(CMAKE_${comp}_COMPILER_PATCH ${CMAKE_MATCH_3})
-endforeach()
+#foreach(comp  ${COMPILERS})
+#  string(REGEX MATCH "([0-9]*)\\.([0-9]*)\\.([0-9]*)"
+#         major ${CMAKE_${comp}_COMPILER_VERSION})
+#  set(CMAKE_${comp}_COMPILER_MAJOR ${CMAKE_MATCH_1})
+#  set(CMAKE_${comp}_COMPILER_MINOR ${CMAKE_MATCH_2})
+#  set(CMAKE_${comp}_COMPILER_PATCH ${CMAKE_MATCH_3})
+#endforeach()
